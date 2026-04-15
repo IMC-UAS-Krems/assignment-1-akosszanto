88831c5581b30574ed3394282ac3806bac68b74f
akosszanto <szantoakos987@gmail.com>
2026-04-15 10:38:12 +0200

    Artists class fixes

--- src/streaming/artists.py
+++ src/streaming/artists.py
@@ -5,7 +5,7 @@ class Artist:
         self.artist_id = artist_id
         self.name = name
         self.genre = genre
-        self.tracks = list[Track]()
+        self.tracks: list[Track] = []
     
     def add_track(self, track: Track) -> None:
         self.tracks.append(track)
